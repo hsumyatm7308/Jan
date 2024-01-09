112d16e866dd638d184ea0e65121c61813033f39
Hsu Myat Moe <hsumyatm7308@gmail.com>
2024-01-10 02:02:13 +0630

    2:02 jan10

---
+++ folder/chatbox/index/app.js
@@ -0,0 +1,9 @@
+
+
+// For Chat 
+const getchatmoredotbtn = document.getElementById('chat_moredot');
+getchatmoredotbtn.addEventListener('click', function () {
+   const getchatmodal = document.getElementById('chat_modal');
+   getchatmodal.classList.toggle('hidden');
+});
+
